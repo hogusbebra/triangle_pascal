10bfc68348fed46bc50a1c855c6777c234e59253
Ilya-Stafeev <stafeev-ilya@bk.ru>
2023-10-04 15:33:12 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Triangle
+{
+    static void Main()
+    {
+        int num;
+        Console.Write("Введите размер матрицы: ");
+        num = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[num, num];
+        int[,] way = new int[num, num];
+
+        for (int i = 0; i < num; i++)
+        {
+            way[i, 0] = 1;
+        }
+        for (int j = 0; j < num; j++)
+        {
+            way[0, j] = 1;
+        }
+
+        for (int i = 1; i < num; i++)
+        {
+            for (int j = 1; j < num; j++)
+            {
+                way[i, j] = way[i - 1, j] + way[i, j - 1];
+            }
+        }
+        Console.WriteLine($"Количество путей: {way[num - 1, num - 1]}");
+    }
+}
